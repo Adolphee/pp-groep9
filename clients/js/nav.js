8b061b251261315adb4aba54d6fc1cb49000aa87
karel <karel.claes@yahoo.com>
2019-05-14 17:25:22 +0200

    update

--- clients/js/nav.js
+++ clients/js/nav.js
@@ -5,8 +5,29 @@ $(document).ready(function () {
         $("#nav").html(res)
     });
 
-$('#dropnav').on("change",function () {
-    let
+$(document).on("change",'#navdrop',function (e) {
+    console.log("change van select werkt");
+    let value = e.target.value;
+    let ajaxPath = "http://10.3.50.56:3009/api/"+value;
+    $.ajax({
+        url: ajaxPath,
+        method: 'GET',
+        dataType: 'json'
+    }).done(function(data){
+        console.log('DONE');
+        console.log(data);
+        // deleten van vorige items
+        $('#catContainer').empty();
+        for (let b of data)
+        {
+
+            $('#catContainer').append(`<div><img src='/pp-groep9/images/${b.product_id}.png'><h3>${b.productnaam}</h3></div>`);
+        }
+
+    }).fail(function(er1, er2){
+        console.log(er1);
+        console.log(er2);
+    });
     
-})
+});
 });
