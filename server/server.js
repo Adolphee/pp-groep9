f7ca2c904ef6f1f95ca42383bedca7ac09cf547b
jarnobogaert9 <jarnobogaert9@gmail.com>
2019-05-16 19:09:52 +0200

    begin pagina server aangepast

--- server/server.js
+++ server/server.js
@@ -29,55 +29,7 @@ app.use('/user', user);
 app.use('/contact', submitForm);
 
 app.get('/', (req, res) => {
-  res.send(`
-    <h2>This is the webserver that interacts with the front-end.</h2>
-    <p>We have the /api route where you can request all the data.</p>
-    <h3>get routes</h3>
-    <ul>
-      <li>api/tenten</li>
-      <li>api/slaapGerief</li>
-      <li>api/kookGerei</li>
-      <li>api/search/"searchTerm"</li>
-      <li>api/getProduct/:id</li>
-      <li>api/getFreeItems/:id</li>
-      <li>api/getAllProducts</li>
-    </ul>
-    <h3>post routes</h3>
-    <ul>
-      <li>
-        api/insert/newAdres
-        <br> gebruik deze data <br> 
-        {
-          "land": "BE",
-          "gemeente": "opwijk",
-          "postcode": "1745",
-          "straat": "steenweg",
-          "huisnummer": "45",
-          "busnummer":""
-        }
-        <br>
-        en heraal de post nog is om te kijken of het al bestaat
-      </li>
-      <br>
-      <li>
-        api/insert/newKlant (bij nieuwe klant moet er altijd een adres meegegeven worden)
-        <br> gebruik deze data <br> 
-        {
-          "voornaam": "olivier",
-          "achternaam": "bogaert",
-          "geboortedatum": "02/05/1973",
-          "land": "be",
-          "gemeente": "lembeek",
-          "postcode": "1502",
-          "straat": "popstraat",
-          "huisnummer": "17",
-          "busnummer": ""
-        }
-        <br>
-        en heraal de post nog is om te kijken of het al bestaat
-      </li>
-    </ul>
-  `);
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 app.listen(PORT, () => {
