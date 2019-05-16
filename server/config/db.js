c28bba8126aca9af6b771c95755c77514a72ae43
jarnobogaert9 <jarnobogaert9@gmail.com>
2019-05-16 09:42:44 +0200

    db connectie van single connection naar een pool verandert om zo het probleem tegen te gaan dat we soms connecteren met de database wnr de connection closed is

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,13 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: '172.20.0.200',
   user: '1819IP_groep09',
   database: '1819IP_groep09',
-  password: 'dimadi'
+  password: 'dimadi',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
-
-db.connect((err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('Connected to database');
-  
-})
 
 module.exports = db;
