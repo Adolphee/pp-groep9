bf7cdd67998a2fd2e3b7b5c51ac11d835d8bf3d6
jarnobogaert9 <jarnobogaert9@gmail.com>
2019-05-17 15:40:12 +0200

    loadPage toegevoegd, load alle producten op index

--- clients/js/cart.js
+++ clients/js/cart.js
@@ -1,11 +1,28 @@
 $(document).ready(() => {
-  // $('#reserveer').click((e) => {
-  //   e.preventDefault();
-  //   console.log('okkkkkk');
-  //   $.ajax({
-  //     url: './modal.html'
-  //   }).done((res) => {
-  //     $('#loadModal').html(res);
-  //   })
-  // });
+  let bestelregels = JSON.parse(sessionStorage.getItem('cart'));
+  let totaalPrijs = 0;
+  let totaalWaarborg = 0;
+  console.log(bestelregels);
+  if (bestelregels != null) {
+    for (const regel of bestelregels) {
+      totaalPrijs += Number(regel.prijs);
+      totaalWaarborg += Number(regel.waarborg);
+      $('#bestelregelsBody').append(`
+      <tr>
+      <td>${regel.productnaam}</td>
+      <td>${regel.prijs}</td>
+      <td>${regel.waarborg}</td>
+      <td><i id="${regel.item_id}" class="fas fa-times cross-cart"></i><td/>
+      </tr>
+      `)
+    }
+    // som van alles tonen
+    $('#totaalPrijs').html(totaalPrijs);
+    $('#totaalWaarborg').html(totaalWaarborg);
+  }
+
+  // Als er op kruisjes word gedrukt
+  $(document).on('click', '.cross-cart', (e) => {
+    console.log(e.target.id);
+  })
 });
