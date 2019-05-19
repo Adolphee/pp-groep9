cda09a016c3d3144994da24488b01b4af8350ced
flipedeus <arne.vandenbroeck@student.ehb.be>
2019-05-19 14:12:12 +0200

    Contact
    Verder uitwerken

--- clients/js/contact.js
+++ clients/js/contact.js
@@ -20,3 +20,29 @@ $(document).ready(() => {
         })
     })
 });
+
+<!-- Naam Validatie -->
+
+function Naam_Validatie() {
+    let naam = document.createElement('input');
+    let input= "input1".value;
+}
+    if (("input".length)>3) {
+        document.getElementById('naam_validatie').innerText = "Correct!";
+        document.getElementById('naam_validatie').style.color = '#7ed913';
+}
+
+<!-- E-mail Validatie -->
+function Email_Validatie() {
+    let email = document.createElement('input');
+    email.type = 'email';
+    email.value = document.getElementById('input2').value;
+
+    if (email.checkValidity()) {
+        document.getElementById('email_validation').innerText = "Correct!";
+        document.getElementById('email_validation').style.color = '#7ed913';
+    } else {
+        document.getElementById('email_validation').innerText = "Fout! Het e-mailadress moet van de vorm: 'example@example.com' zijn";
+        document.getElementById('email_validation').style.color = "#F20000";
+    }
+}
